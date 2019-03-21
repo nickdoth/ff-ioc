4395dc1319b512ddf79acb7939b5117bb93e0943
Nick Ng <nick.ng@hkday.com.hk>
2019-03-22 01:28:11 +0800

    Detect recursive injection and throw error

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,26 @@ type ProviderMap<T extends { [k: string]: any }> = {
 export function createContainer<T>(providerMap: ProviderMap<T>): T {
     const internalContainer: any = {};
     const container: T = {} as T;
+    const injecting: { [p: string]: boolean } = {};
 
     for (const p in providerMap) {
         if (providerMap.hasOwnProperty(p)) {
             Object.defineProperty(container, p, {
                 get: () => {
+                    if (injecting[p]) {
+                        throw new Error(`Error while injecting ${p}: recursive reference detected`);
+                    }
+                    
                     try {
                         if (!internalContainer[p]) {
+                            injecting[p] = true;
                             internalContainer[p] = providerMap[p](container);
+                            injecting[p] = false;
                         }
                         return internalContainer[p];
                     } catch (e) {
                         console.error(`Error while injecting ${p}:`);
+                        injecting[p] = false;
                         throw e;
                     }
                 },
